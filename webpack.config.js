3fefa7079f8c7b7c95e3a5de4ac77f5c448bb1a2
Jaylonnet <jaylonwingfield10@gmail.com>
2024-03-13 15:20:25 +0000

    Initial project setup
    Initial setup includes:
    - Node
    - Webpack
    - Basic index.html, indes.js & style.css

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'To-Do-Manager',
+            filename: 'index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
